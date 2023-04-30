a422dcdb5a2b9d53498c3610f894aabe21d4f7b3
fo-pluto <2818151216@qq.com>
2023-04-30 17:09:12 +0800

    start 干

---
+++ ArmorTrack/armor_track.h
@@ -0,0 +1,33 @@
+/**
+ * @author 可莉不知道哦
+ * @brief 测试预测，感觉预测可以做的更好，所以重新开了一个类用于研究
+ * @date 2023/4/30
+*/
+
+#ifndef ARMOR_TRACK
+#include "Main/headfiles.h"
+#include "Detector/yolov5.h"
+
+
+class armor_track{
+    public:
+
+
+    private:
+
+    public:
+    /**
+     * 空构造函数
+    */
+    anmor_track(){}
+    /**
+     * 带参数构造函数
+    */
+
+    /**
+     * 
+    */
+
+}
+
+#endif // !ARMOR_TRACK
