9a76d0f1ccf103533b1d0dcc689a270043a30505
Fo_Pluto <94886294+FOPluto@users.noreply.github.com>
2023-05-01 09:47:39 +0800

    Update armor_detector.cpp

--- YSU_2022_stentry/ArmorDetector/armor_detector.cpp
+++ YSU_2022_stentry/ArmorDetector/armor_detector.cpp
@@ -1,65 +1,60 @@
+#include "Main/headfiles.h"
+#include "ArmorDetector/armor_detector.h"
+#include "Pose/angle_solver.h"
+#include  <time.h>
+
+ #define DEBUG 1
+
+//由于sort函数的第三参数不属于类内，因此需要使用全局变量，全局变量初始化区
+float ArmorDetector:: hero_zjb_ratio_min=3.9;
+float ArmorDetector::hero_zjb_ratio_max=4.1;
+int  ArmorDetector:: record_history_num=5;
+float ArmorDetector::score_of_hero=1;
+float ArmorDetector::score_of_area=1;
+float ArmorDetector::score_of_last=1;
+
+vector<cv::RotatedRect> ArmorDetector::record_history_arr;
 
 
-#include "kalman.h"
-
-vector<Rect_VectorPoint> ArmorDetector::record_history_arr;
-
-
-
-/**
-  * @name   kalmanCreate
-  * @brief  锟斤拷锟斤拷一锟斤拷锟斤拷锟斤拷锟斤拷锟剿诧拷锟斤拷
-  * @param  p:  锟剿诧拷锟斤拷
-  *         T_Q:系统锟斤拷锟斤拷协锟斤拷锟斤拷
-  *         T_R:锟斤拷锟斤拷锟斤拷锟斤拷协锟斤拷锟斤拷
-  *
-  * @retval none
-  * @attention 	R锟教讹拷锟斤拷Q越锟襟，达拷锟斤拷越锟斤拷锟轿诧拷锟斤拷值锟斤拷Q锟斤拷锟斤拷锟斤拷锟街伙拷貌锟斤拷锟街�
-  *		       		锟斤拷之锟斤拷Q越小锟斤拷锟斤拷越锟斤拷锟斤拷模锟斤拷预锟斤拷值锟斤拷Q为锟斤拷锟斤拷锟斤拷只锟斤拷模锟斤拷预锟斤拷
-  */
-void Kalman_t::KalmanInit(double T_Q,double T_R)
+float get_angle(const cv::Point2f &a,const cv::Point2f &b)
 {
-    X_last = (double)0;
-    P_last = 0;
-    Q = T_Q;
-    R = T_R;
-    A = 1;
-    B = 0;
-    H = 1;
-    X_mid = X_last;
+    float dx=a.x-b.x;
+    if(fabs(dx)<0.1) return -90;
+    float dy=a.y-b.y;
+    return atanf(dy/dx)*180/CV_PI;
 }
 
-/**
-  * @name   KalmanFilter
-  * @brief  锟斤拷锟斤拷锟斤拷锟剿诧拷锟斤拷
-  * @param  p:  锟剿诧拷锟斤拷
-  *         dat:锟斤拷锟剿诧拷锟斤拷锟斤拷
-  * @retval 锟剿诧拷锟斤拷锟斤拷锟斤拷锟�
-  * @attention Z(k)锟斤拷系统锟斤拷锟斤拷,锟斤拷锟斤拷锟斤拷值   X(k|k)锟角匡拷锟斤拷锟斤拷锟剿诧拷锟斤拷锟街�,锟斤拷锟斤拷锟斤拷锟斤拷锟�
-  *            A=1
-		B=0 H=1 I=1  W(K)  V(k)锟角革拷斯锟斤拷锟斤拷锟斤拷,锟斤拷锟斤拷锟节诧拷锟斤拷值锟斤拷锟斤拷,锟斤拷锟皆诧拷锟矫癸拷
-  *            锟斤拷锟斤拷锟角匡拷锟斤拷锟斤拷锟斤拷5锟斤拷锟斤拷锟侥癸拷式
-  *            一锟斤拷H'锟斤拷为锟斤拷锟斤拷锟斤拷,锟斤拷锟斤拷为转锟矫撅拷锟斤拷
-  */
-
-double Kalman_t::KalmanFilter(double dat)
+float get_dis ( const cv::Point2f &a,const cv::Point2f &b)
 {
-    X_mid =A*X_last;                     //锟劫度讹拷应锟斤拷式(1)    x(k|k-1) = A*X(k-1|k-1)+B*U(k)+W(K)
-    P_mid = A*P_last+Q;               //锟劫度讹拷应锟斤拷式(2)    p(k|k-1) = A*p(k-1|k-1)*A'+Q
-    kg = P_mid/(P_mid+R);             //锟劫度讹拷应锟斤拷式(4)    kg(k) = p(k|k-1)*H'/(H*p(k|k-1)*H'+R)
-    X_now = X_mid+kg*(dat-X_mid);     //锟劫度讹拷应锟斤拷式(3)    x(k|k) = X(k|k-1)+kg(k)*(Z(k)-H*X(k|k-1))
-    P_now = (1-kg)*P_mid;                //锟劫度讹拷应锟斤拷式(5)    p(k|k) = (I-kg(k)*H)*P(k|k-1)
-    P_last = P_now;                         //状态锟斤拷锟斤拷
-    X_last = X_now;
-    return X_now;							  //锟斤拷锟皆わ拷锟斤拷锟絰(k|k)
+    return sqrtf(pow(a.x-b.x,2)+pow(a.y-b.y,2));
 }
-    cv::FileStorage fr;
-    fr.open(file_path,cv::FileStorage::READ);
-    while(!fr.isOpened()){
-        cout<<"armor_xml floading failed..."<<endl;
-        fr=cv::FileStorage(file_path,cv::FileStorage::READ);
-        fr.open(file_path,cv::FileStorage::READ);
-    }
+
+ArmorDetector::ArmorDetector()://  1
+    p_svm(std::make_unique<YSU_SVM>()),
+    blue_color_threshold(120),
+    red_color_threshold(0),
+    max_g_dConArea(10000),
+    min_g_dConArea(200),
+    max_angle_abs(20),
+    max_center_y(40),
+    get_data_fps(50)
+{}
+
+
+
+void ArmorDetector::InitArmor()
+{
+    //cout<<"装甲板检测初始化成功！"<<endl;
+    cout<<"armor_detector init begin"<<endl;
+
+         string file_path="../xml_path/armor_limited.xml";
+         cv::FileStorage fr;
+         fr.open(file_path,cv::FileStorage::READ);
+         while(!fr.isOpened()){
+               cout<<"armor_xml floading failed..."<<endl;
+              fr=cv::FileStorage(file_path,cv::FileStorage::READ);
+              fr.open(file_path,cv::FileStorage::READ);
+         }
 
         fr["contour_area_min"]>>contour_area_min;
         fr["contour_area_max"]>>contour_area_max;
@@ -90,20 +85,14 @@ double Kalman_t::KalmanFilter(double dat)
 
         fr["wu_cha_yun_xu"]>>wu_cha_yun_xu;
         fr.release();
-cout<<"armor_xml loading finished"<<endl;
-        // 卡尔曼相关
-         for(int i=0;i<8;i++){
-             p_kal.emplace_back(std::make_unique<Kalman>());
-             p_kal[i]->Kalman_init(Kalman_Q,Kalman_R);
-         }
-        // // 初始化SVM函数，按道理来说弃用
-        //  p_svm->InitSVM();
+        cout<<"armor_xml loading finished"<<endl;
+
+         p_svm->InitSVM();
 
         record_history_arr.clear();
-        // 这个部分可能会消耗比较多的时间, 但是是正常现象
-        // 深度学习加载模型
         this->yolov5_detector_ = new Yolov5("../model/model/opt-0527-001.xml", "../model/model/opt-0527-001.bin", 416, 416); // 创建yolov5detector对象
         this->yolov5_detector_->init_yolov5_detector(); // init yolov5 detector 模型加载部分
+        // 这个部分可能会消耗比较多的时间, 但是是正常现象
         cout<<"armor_detector init finished"<<endl;
 }
 
